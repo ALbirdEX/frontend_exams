5fe6c1da382bb73f70e82771ebef1b4a3ddb60c9
ALbirdEX <alex123890@gmail.com>
2023-03-28 20:31:04 +0300

    TODO

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from "./Todo";
+import {useAppDispatch, useAppSelector} from "./state/store";
+import {Loader} from "./Loader";
+import {changeTodoTC, deleteTodolistTC} from "./state/todo-reducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+
+    const dispatch = useAppDispatch()
+
+    const {todo, isLoading, error} = useAppSelector(state => state.todos)
+
+    const onChangeStatusHandler = (id: string, completed: boolean) => {
+        dispatch(changeTodoTC(id, completed))
+    }
+    const deleteTodolistHandler = (id: string) => {
+        dispatch(deleteTodolistTC(id))
+    }
+    return (
+        <div style={{padding: "300px"}}>
+            {isLoading && <Loader/>}
+            {!!error && <div style={{color: "red"}}>{error}</div>}
+            <div>
+                <Todo/>
+                {todo.map((tl) => <div key={tl.id} style={tl.completed ? {opacity: 0.3} : {}}>
+
+                    <input type={"checkbox"}
+                           checked={tl.completed}
+                           onChange={() => onChangeStatusHandler(tl.id, !tl.completed)}/>
+
+                    {tl.title}
+
+                    <button onClick={()=> deleteTodolistHandler(tl.id)}>Х</button>
+                </div>)}
+
+            </div>
+        </div>
+    )
 }
-
-export default App;
